c2fcaa83613fbd25d294554b11f6209acb88edbc
sebastianortegaanaya <sebastianortegaanaya@gmail.com>
2026-09-13 12:01:36 -0500

    feat: definir metodos a las clases

--- clases.py
+++ clases.py
@@ -6,11 +6,13 @@ class casa:
         self.barrio = barrio
         self.Municipio = Municipio
         self.estrato = estrato
-Casa1=  casa(15, 27, "Decima", "Betania", "LosPatios", 4)
+    
+    def imp(self):
+        print(f"Estrato: {self.estrato}, Numero de casa: {self.numerocasa}, Avenida: {self.avenida}, Barrio: {self.barrio}, Municipio: {self.Municipio}, Calle: {self.calle}")
 
-print(Casa1.estrato)
-print(Casa1.avenida)
-print(Casa1.barrio)
-print(Casa1.calle)
-print(Casa1.Municipio)
-print(Casa1.numerocasa)
+
+Casa1=  casa(15, 27, "Decima", "Betania", "LosPatios", 4)
+Casa2=  casa(15, 27, "quinta", "El sol", "LosPatios", 1)
+
+Casa1.imp()
+Casa2.imp()
